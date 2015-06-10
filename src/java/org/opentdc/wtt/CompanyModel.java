3b53aba7cc15c0a01b16c5597ae899459841b1df
Bruno Kaiser <bruno@bkaiser.ch>
2015-06-10 21:58:58 +0200

    made attribute title mandatory

--- src/java/org/opentdc/wtt/CompanyModel.java
+++ src/java/org/opentdc/wtt/CompanyModel.java
@@ -38,7 +38,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 @XmlRootElement
 public class CompanyModel {
 	private String id;			// sortable
-	private String title;
+	private String title;		// mandatory
 	private String description;
 	private Date createdAt;
 	private String createdBy;
